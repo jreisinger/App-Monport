0ea8dc84ad933c137d9ec1cc3c7013814cadded9
Jozef Reisinger <jozef.reisinger@gmail.com>
2016-02-15 08:03:10 +0000

    del command (WIP)

---
+++ lib/App/Monport/Command/del.pm
@@ -0,0 +1,61 @@
+# ABSTRACT: delete base scans
+package App::Monport::Command::del;
+use App::Monport -command;
+use App::Monport::Nmap;
+
+=head1 DESCRIPTION
+
+del sub-command. See App::Cmd::Tutorial for more.
+
+=cut
+
+=head1 METHODS
+
+=head2 usage_desc()
+
+Usage description.
+
+=cut
+
+sub usage_desc { "del %o" }
+
+=head2 options()
+
+Options.
+
+=cut
+
+sub options {
+}
+
+=head2 validate()
+
+Validate the command options and arguments.
+
+=cut
+
+sub validate {
+  my ($self, $opt, $args) = @_;
+
+  $self->usage_error("no scan to delete") unless @$args;
+}
+
+=head2 execute()
+
+Run the command.
+
+=cut
+
+sub execute {
+  my ($self, $opt, $args) = @_;
+
+  set_vars($opt->name );
+
+  if ($opt->print) {
+      print_basescan();
+  } else {
+      del_basescans();
+  }
+}
+
+1;
