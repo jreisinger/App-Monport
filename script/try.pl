543141e80ff84936b115e5712fb0d37133172fdd
Jozef Reisinger <jozef.reisinger@gmail.com>
2016-11-26 14:15:29 +0100

    detect newly open/closed ports

--- script/try.pl
+++ script/try.pl
@@ -2,18 +2,42 @@
 use strict;
 use warnings;
 use IO::Socket;
+use Storable;
+
+my $file = q/open_ports.data/;
+my $open_ports = retrieve($file) if -f $file;
+
+#use Data::Dumper;
+#print Dumper $open_ports;
 
 for my $host (@ARGV) {
     my $open = scan_ports($host);
-    print "$host\n";
-    print "    $_\n" for @$open;
+    print "$host: @$open\n";
+
+    if ( exists $open_ports->{$host} ) {
+
+        # a new port got open?
+        for my $port (@$open) {
+            print "port opened $host:$port\n"
+              if not grep $port == $_, @{ $open_ports->{$host} };
+        }
+
+        # an open port got closed?
+        for my $port ( @{ $open_ports->{$host} } ) {
+            print "port closed $host:$port\n"
+              if not grep $port == $_, @$open;
+        }
+    }
+
+    $open_ports->{$host} = $open;
+    store $open_ports, $file;
 }
 
 sub scan_ports {
     my $host = shift;
 
     my @open;
-    for my $port (qw( 22 80 443 8000 8030 8020 8089 9997 )) {
+    for my $port (qw( 22 80 443 1234 8000 8030 8020 8089 9997 )) {
 
         my $socket = IO::Socket::INET->new(
             PeerAddr => $host,
@@ -25,7 +49,7 @@ sub scan_ports {
 
         if ($socket) {
             push @open, $port;
-            shutdown($socket, 2);
+            shutdown( $socket, 2 );
         }
     }
 
