61a949f2f5858ef0c8801a33cc0234271f723ab2
Jozef Reisinger <jozef.reisinger@gmail.com>
2016-12-03 21:32:43 +0100

    clean up code base

--- lib/App/Monport.pm
+++ lib/App/Monport.pm
@@ -18,17 +18,84 @@ App::Monport - Monitor network ports for changes
 
 =head1 SYNOPSIS
 
- $ monport
+ $ monport localhost scanme.nmap.org  # generate the configuration file
+
+ $ monport  # compare against the configuration file
 
 =head1 DESCRIPTION
 
-Use this application to find out whether some new ports have been opened or
+Use this module to find out whether some new ports have been opened or
 existing ones have been closed. New open ports mean bigger attack surface and
 consequently higher security risk. On the other hand if a port gets closed it
 might indicate a problem with a network service.
 
 The application works by comparing the actual state of ports (open or closed)
 with the expected state defined in the configuration file.
+
+=head1 FUNCTIONS
+
+See F<bin/monport> for how to use these functions.
+
+=head2 scan_ports($host)
+
+Return an array reference containing list of ports open on $host.
+
+=cut
+
+sub scan_ports {
+    my $host = shift;
+
+    my $np = new Nmap::Parser;
+
+    #runs the nmap command with hosts and parses it automagically
+    my $nmap = nmap_path();
+    $np->parsescan( $nmap, '', $host );
+
+    my ($h) = $np->all_hosts();
+    my @ports = $h->tcp_ports(q(open));
+
+    return \@ports;
+}
+
+#sub scan_ports {
+#    my $host = shift;
+#    my @open;
+#    print "scanning $host: \n";
+#    my $ports = default_ports();
+#    for my $port (@$ports) {
+#        my $socket = IO::Socket::INET->new(
+#            PeerAddr => $host,
+#            PeerPort => $port,
+#            Proto    => 'tcp',
+#            Type     => SOCK_STREAM,
+#            Timeout  => 1,
+#        );
+#
+#        if ($socket) {
+#            push @open, $port;
+#            shutdown( $socket, 2 );
+#        }
+#    }
+#
+#    return \@open;
+#}
+
+=head2 nmap_path()
+
+Return absolute path to nmap executable or die.
+
+=cut
+
+sub nmap_path {
+    my @paths = qw(
+      /usr/bin/nmap
+      /usr/local/bin/nmap
+    );
+    for my $p (@paths) {
+        return $p if -x $p;
+    }
+    die "can't find exacutable nmap; searched @paths\n";
+}
 
 =head1 INSTALLATION
 
@@ -62,72 +129,9 @@ Jozef Reisinger, E<lt>reisinge@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2015 by Jozef Reisinger.
+This software is copyright (c) 2015-2016 by Jozef Reisinger.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
-
-=cut
-
-=head2 scan_ports($host)
-
-Return an array reference containing list of ports open on $host.
-
-=cut
-
-sub scan_ports {
-    my $host = shift;
-
-    my $np = new Nmap::Parser;
-
-    #runs the nmap command with hosts and parses it automagically
-    my $nmap = nmap_path();
-    $np->parsescan( $nmap, '', $host );
-
-    my ($h) = $np->all_hosts();
-    my @ports = $h->tcp_ports(q(open));
-
-    return \@ports;
-}
-
-=head2 nmap_path()
-
-Return absolute path to nmap executable or die.
-
-=cut
-
-sub nmap_path {
-    my @paths = qw(
-      /usr/bin/nmap
-      /usr/local/bin/nmap
-    );
-    for my $p (@paths) {
-        return $p if -x $p;
-    }
-    die "can't find exacutable nmap; searched @paths\n";
-}
-
-#sub scan_ports {
-#    my $host = shift;
-#    my @open;
-#    print "scanning $host: \n";
-#    my $ports = default_ports();
-#    for my $port (@$ports) {
-#        my $socket = IO::Socket::INET->new(
-#            PeerAddr => $host,
-#            PeerPort => $port,
-#            Proto    => 'tcp',
-#            Type     => SOCK_STREAM,
-#            Timeout  => 1,
-#        );
-#
-#        if ($socket) {
-#            push @open, $port;
-#            shutdown( $socket, 2 );
-#        }
-#    }
-#
-#    return \@open;
-#}
 
 1;
--- t/monport.t
+++ t/monport.t
@@ -3,11 +3,10 @@ use 5.006;
 use strict;
 use warnings FATAL => 'all';
 use Test::More;
-use IO::Socket;
 use App::Monport;
 
 my $host = q(scanme.nmap.org);
-my $open = App::Monport::scan_ports($host);
+my $open = scan_ports($host);
 for my $expected (qw(22 80)) {
     ok( grep( $expected == $_, @$open ), "$host has port $expected open" );
 }
